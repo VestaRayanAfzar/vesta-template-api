146ce5009e86788dc1baf10bbe0656b1d6a4578a
hbtb <bishehsary@gmail.com>
2017-04-23 17:09:15 +0000

    removing gulp-typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,58 @@
 let gulp = require('gulp');
 let path = require('path');
-let config = require('./resources/gulp/config');
-let {dir} = config;
+let http = require('http');
+let server = require('gulp-develop-server');
+let chalk = require('chalk');
+
+let root = __dirname;
+
+const dir = {
+    root: root,
+    build: path.join(root, 'vesta'),
+    buildServer: path.join(root, 'vesta/server'),
+};
+
+const debug = {type: 'debug', ports: {debug: 5858, inspect: 9229}, address: '192.168.99.100'};
+const port = {api: 3000};
 
 
-/** sets setting.production to true for deploy tasks */
-gulp.task('production', () => {
-    setting.production = true;
-});
+/**
+ * This tasks is called by npm script for starting api server in docker env
+ */
+gulp.task('api', function () {
+    let delay = 500, debuggerDelay = 500, timer, debuggerTimer;
+    let serverDirectory = `${dir.build}/server`;
+    let port = debug.ports[debug.type];
+    server.listen({path: `${serverDirectory}/app.js`, execArgv: [`--${debug.type}=0.0.0.0:${port}`]});
+    let isInspect = debug.type === 'inspect';
+    isInspect && loadDebugger();
+    gulp.watch([`${serverDirectory}/**/*.js`, `!${serverDirectory}/static/**/*.js`], function () {
+        clearTimeout(timer);
+        clearTimeout(debuggerTimer);
+        timer = setTimeout(() => {
+            server.restart();
+            isInspect && loadDebugger();
+        }, delay);
+    });
 
-/** Loading api tasks foreach target */
-let [tasks, watches] = loadTasks(['api']);
-gulp.task(`dev:api`, tasks.concat(watches));
-gulp.task(`deploy:api`, ['production'].concat(tasks));
+    function loadDebugger() {
+        debuggerTimer = setTimeout(launchInspector, debuggerDelay);
 
-/** Loading tasks from each modules*/
-function loadTasks(modules) {
-    let tasks = [],
-        watches = [];
-
-    for (let i = 0, il = modules.length; i < il; ++i) {
-        let result = require(path.join(dir.gulp, modules[i]))(config);
-        if (result.tasks) {
-            tasks = tasks.concat(result.tasks);
-        }
-        if (result.watch) {
-            watches = watches.concat(result.watch);
+        function launchInspector() {
+            http.get(`http://${debug.address}:${debug.ports.inspect}/json`, res => {
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', chunk => {
+                    rawData += chunk;
+                });
+                res.on('end', () => {
+                    let data = JSON.parse(rawData);
+                    let url = data[0]['devtoolsFrontendUrl'];
+                    let regex = new RegExp(`&ws=[^:]+:${debug.ports.inspect}\/`);
+                    url = url.replace(regex, `&ws=${debug.address}:${debug.ports.inspect}/`);
+                    process.stdout.write(`\n\nInspect URL: "${chalk.cyan(url)}"\n\n`);
+                })
+            }).on('error', err => process.stderr.write(err.message));
         }
     }
-    return [tasks, watches];
-}
+});
