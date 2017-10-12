948e6a97c0a3ac4b1e8ad40f957088a079d52c27
Linus Decker Jr <wizer0x64@Wizer0x64Pro.local>
2017-10-12 08:40:00 -0400

    retry if initiation failed

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,30 @@
 import {config} from "./config/config";
 import {ServerApp} from "./ServerApp";
 
-(async function () {
+const MAX_TRY_COUNT = 3;
+const TRY_INTERVAL = 5000;
+let tryCounter = 1;
+
+(async function run() {
     try {
         let server = new ServerApp(config);
         await server.init();
         await server.start();
-        console.log('server booted at ', new Date().toString())
+        console.log('Server booted at ', new Date().toString());
     } catch (err) {
-        console.error('server initiation error: ', err);
+        ++tryCounter;
+        console.error('Server initiation error: ', err);
+        if (tryCounter <= MAX_TRY_COUNT) {
+            // increasing the interval in case of repetitive errors
+            let nextTryInterval = TRY_INTERVAL * tryCounter;
+            console.warn(`A retry effort will occurred in ${nextTryInterval / 1000}s`);
+            setTimeout(async () => {
+                console.warn(`Restarting server initiation process [try #${tryCounter}]...`);
+                await run();
+            }, nextTryInterval);
+        } else {
+            console.error('MAX_TRY_COUNT reached; exiting server...');
         process.exit(1);
+    	}
     }
 })();
